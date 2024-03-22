057fa4e617d6fdb06e6e2773d10a2f02ab69422b
Darkhan Zhursin <daratechn@gmail.com>
2024-03-21 22:54:51 -0500

    done

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,23 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> list = new LinkedList<>();
+        for (Map.Entry<String, Set<String>> entry : projects.entrySet()) {
+            if (entry.getValue().contains(developer)) {
+                list.add(entry.getKey());
+            }
+        }
+        if (!list.isEmpty()) {
+            list.sort((o1, o2) -> {
+                if (o1.length() == o2.length()) {
+                    return -o1.compareToIgnoreCase(o2);
+                }
+                return o2.length() - o1.length();
+            });
+        }
+        return list;
     }
 }
